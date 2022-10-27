91404016fb30fdc04146e7336c9963f8d0b39ae4
Vahe Yavrumian <vaheyavrumyan@gmail.com>
2022-10-27 16:01:29 +0400

    Adjust for google cloud storage

--- app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express'),
-    app = express()
-
-app.use(express.static('./public'))
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on ${process.env.PORT}`);
-})
